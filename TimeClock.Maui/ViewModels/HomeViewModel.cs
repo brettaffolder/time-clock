64b65e72f36b175f9b8b623480cbb5ab59c32551
Brett Affolder <brettaffolder@outlook.com>
2026-07-26 19:46:21 -0400

    some home page setup

--- TimeClock.Maui/ViewModels/HomeViewModel.cs
+++ TimeClock.Maui/ViewModels/HomeViewModel.cs
@@ -1,8 +1,61 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using TimeClock.Core;
+using TimeClock.Core.Models;
+using TimeClock.Maui.Contracts;
 
 namespace TimeClock.Maui.ViewModels;
 
-public partial class HomeViewModel : ObservableObject
+public partial class HomeViewModel(
+    IEmployeeService employee,
+    ITimeEntryService timeEntry) : ObservableObject
 {
+    private readonly IEmployeeService _employee = employee;
+    private readonly ITimeEntryService _timeEntry = timeEntry;
 
+    public Page? Page { get; set; }
+
+    [ObservableProperty]
+    public partial string Time { get; set; } = DateTime.Now.ToString("h:mm tt");
+
+    [ObservableProperty]
+    public partial ObservableCollection<DataObjects.TimeClock> TimeClocks { get; set; } = [];
+
+    [RelayCommand]
+    private async Task Refresh()
+    {
+        TimeClocks.Clear();
+
+        Result<IEnumerable<Employee>> employeesResult = await _employee.AllAsync();
+        if (employeesResult.IsError || employeesResult.Value is null)
+        {
+            if (Page is not null)
+            {
+                await Page.DisplayAlertAsync("Error", employeesResult.Error?.Message, "Close");
+            }
+
+            return;
+        }
+
+        Result<IEnumerable<TimeEntry>> timeEntriesResult = await _timeEntry.AllAsync();
+        if (timeEntriesResult.IsError || timeEntriesResult.Value is null)
+        {
+            if (Page is not null)
+            {
+                await Page.DisplayAlertAsync("Error", timeEntriesResult.Error?.Message, "Close");
+            }
+
+            return;
+        }
+
+        foreach (Employee employee in employeesResult.Value)
+        {
+            IEnumerable<TimeEntry> timeEntries = timeEntriesResult.Value.Where(t => t.Employee?.Id == employee.Id);
+
+            TimeClocks.Add(new DataObjects.TimeClock(_employee, _timeEntry, employee, [.. timeEntries]));
+        }
+    }
 }
--- TimeClock.Maui/Views/HomePage.xaml.cs
+++ TimeClock.Maui/Views/HomePage.xaml.cs
@@ -4,12 +4,30 @@ namespace TimeClock.Maui.Views;
 
 public partial class HomePage : ContentPage
 {
-    public HomeViewModel ViewModel { get; }
+    private readonly HomeViewModel _viewModel;
 
     public HomePage(HomeViewModel viewModel)
 	{
 		InitializeComponent();
 
-        ViewModel = viewModel;
-	}
+        _viewModel = viewModel;
+        BindingContext = viewModel;
+
+        viewModel.Page = this;
+
+        IDispatcherTimer clockTimer = Application.Current!.Dispatcher.CreateTimer();
+        clockTimer.Interval = TimeSpan.FromSeconds(1);
+        clockTimer.Tick += (_, _) => _viewModel.Time = DateTime.Now.ToString("h:mm tt");
+        clockTimer.Start();
+
+        IDispatcherTimer refreshTimer = Application.Current!.Dispatcher.CreateTimer();
+        refreshTimer.Interval = TimeSpan.FromMinutes(1);
+        refreshTimer.Tick += async (_, _) => await _viewModel.RefreshCommand.ExecuteAsync(null);
+        refreshTimer.Start();
+    }
+
+    protected override async void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        await _viewModel.RefreshCommand.ExecuteAsync(null);
+    }
 }
